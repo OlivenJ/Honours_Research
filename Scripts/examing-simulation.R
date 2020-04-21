2b08b1a4cb209693370ea2d62df6a92054aa092a
zhiyuan jiang <olivenjiang@outlook.com>
2020-04-21 20:26:41 +1000

    no message

--- Scripts/examing-simulation.R
+++ Scripts/examing-simulation.R
@@ -1,3 +1,5 @@
+library(mvtnorm)
+
 set.seed(19970628)
 
 t <- 120
@@ -7,15 +9,29 @@ n <- 500
 #How many independent cross-section observation we have. Since in the real data scenario
 #we will use S&P 500, so we set the sample size to 500 to mimic it.
 k <- 30
+# the number of factors
+alpha <- c(1, rep(0.55, k))
+#the factor strength, with the first term as market factor strength, which equals to 1
+#represents that market factor influence all the strocks, the rest 30 factors have 
+#strength 0.55, which is pretty low.
+rf <- 0.04
+#randomly assigned risk free rate as 4%
+Sigma <- diag(1, k)
+Y <- c(0.1, rmvnorm(1,rep(0, nrow(Sigma)), Sigma))
+#random generate factor list, the first one is the market facotr which equals to 
+#the difference between makret return and risk free return
 
-alpha <- 0.55
-#The inital factor strengh setting, alpha equals to 0.55 (indicates weak factor)
+#assign the factor strengh, for all the factor, the strengh is 0.55 (weak factor), and
+#for the only market factor, is strong (alpha = 1)
 
-epsilon <- rnorm(t, 0, 1)
-mu <- runif(t, epsilon-0.2, epsilon + 0.2)
+sig_fact <- as.integer(k^alpha)
+#Indicates under the factor strength setting, how many factor laodings are significnatly
+#different from 0. We only take the integer part.
+
+mu_var = 0.71
+mu <- matrix(runif(t*(k+1),mu_var-0.2, mu_var + 0.2),nrow = t, ncol = k+1)
 hist(mu)
 #Generate the error, for the baseline setting, the error term epsilon will be iid normal
 #0 mean and 1 variance.
 
-return <- runif(t,-0.5,0.5)
-hist(return)
+constant <- runif(t, -0.5, 0.5)
