071ac2649f542855e08f0516039a172ce44d0718
zhiyuan jiang <olivenjiang@outlook.com>
2020-05-01 21:23:46 +1000

    implied stepwise regression, adjust critical value (need confirm)

--- Scripts/examing-simulation.R
+++ Scripts/examing-simulation.R
@@ -18,7 +18,7 @@ constant <- matrix(runif(n*t, -0.5, 0.5), nrow = n, ncol = t)
 
 error <- matrix(rnorm(n*t, 0,1), nrow = n, ncol = t)
 #Assume the error term follows a standard normal distribution
-alpha = 1
+alpha = 0.55
 strength <- c( rep(alpha, k),1)
 #the factor strength, with the last term as market factor strength, which equals to 1
 #represents that market factor influence all the strocks, the rest 30 factors have 
@@ -29,7 +29,12 @@ Sigma <- diag(1,k+1)
 
 factor <- t(rmvnorm(t,mean = rep(0, k+1), Sigma))
 
-market_factor <- matrix(factor[nrow(factor),])
+
+
+market_factor <- t(matrix(factor[nrow(factor),]))
+colnames(market_factor) <- paste0("m", 1:t)
+market_factor <- as_tibble(market_factor)%>% 
+  pivot_longer(c(paste0("m", 1:t)), names_to = "month", values_to = "market_factor_val")
 #random generate factor list, the last one is the market facotr which by definition
 #equals to the difference between makret return and risk free return
 
@@ -53,13 +58,11 @@ loading_generate<- function(zero_amount){
   }
   return(loading)
 }
-
 loading <- loading_generate(sig_count)
-
+##Package the generate factor and return into tibble
 return <-  constant + loading%*%factor + error
 colnames(return) <- paste0("m", 1:t)
 return <- as_tibble(return) %>% mutate(stock = 1:n) %>% select(stock, everything())
-
 return <- return %>%
   pivot_longer(c(paste0("m", 1:t)), names_to = "month", values_to = "return")
 
@@ -69,26 +72,55 @@ factor <- factor %>%
   pivot_longer(c(paste0("m", 1:t)), names_to = "month", values_to = "factor_val")
 
 combine_table <- right_join(return, factor)
-
+#==============+==============+==============+==============+==============+==============+==============+#
+##The setting of only strong factor.
 factor_group <- combine_table %>% group_by(factor,stock) %>% nest()
 
-
-
-factor_regress <-function(data){
+single_market_regress <-function(data){
   tidy(lm(return ~ factor_val, data))
 }
 
-factor_group <- factor_group %>% 
-  mutate(coefficient = map(data, factor_regress))
+single_market_factor <- factor_group %>% 
+  filter(factor == k+1) %>% 
+  mutate(coefficient = map(data, single_market_regress)) %>% 
+  unnest(coefficient)
 
-
-
-strength_result <- factor_group %>% unnest(coefficient) %>%
+single_market_result <- single_market_factor %>%
   select(-data) %>% filter(term != "(Intercept)") %>% 
   group_by(factor) %>% filter(statistic > adj_stat) %>% 
-  summarize(alpha_hat =(log(n())/log(n)))
+  summarize(alpha_hat =(log(n())/log(n))) 
 
-summary(strength_result)
-strength_result %>% filter(alpha_hat >= 0.99) %>% 
-  summarize(n())
+##The result is consistent with the inital setting, all the factors loading among 500 assets are significant,
+##The corresponding strength is 1, indicates the market factor influence across all assets.
+#==============+==============+==============+==============+==============+==============+==============+#
+##The setting of strong factor and one other factor.
+two_factors <- combine_table %>% 
+  filter(factor != k+1) %>% 
+  right_join(market_factor, by = 'month') %>% 
+  select(stock, factor,return, market_factor_val, factor_val)
 
+two_factor_group<- two_factors %>% 
+  group_by(stock,factor) %>% nest()
+
+two_factor_regress <-function(data){
+  tidy(lm(return ~ factor_val + market_factor_val, data))
+}
+
+two_factor <- two_factor_group %>% 
+  mutate(coefficient = map(data, two_factor_regress)) %>% 
+  unnest(coefficient)
+
+  two_factor_result <- two_factor %>% 
+  select(-data) %>% filter(term == "factor_val" ) %>% 
+    group_by(factor) %>% filter(statistic > adj_stat) %>% 
+    summarize(alpha_hat =(log(n())/log(n))) 
+
+  two_factor_result %>% arrange(alpha_hat)
+  summary(two_factor_result)
+  
+  two_factor %>% select(-data) %>% 
+    filter(term == "market_factor_val") %>% 
+    group_by(factor) %>% 
+    filter(statistic > adj_stat) %>% 
+    summarize(alpha_hat =(log(n())/log(n))) %>% 
+    arrange(alpha_hat)
