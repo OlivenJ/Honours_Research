0752081d113b67c8e4e93a93b1760394f8dcddbd
zhiyuan jiang <olivenjiang@outlook.com>
2020-05-04 21:16:16 +1000

    added comment

--- Scripts/examing-simulation.R
+++ Scripts/examing-simulation.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(broom)
 library(mvtnorm)
-library(modeerndive)
+
 
 set.seed(19890604)
 
@@ -11,8 +11,9 @@ t <- 120
 n <- 500
 #How many independent cross-section observation we have. Since in the real data scenario
 #we will use S&P 500, so we set the sample size to 500 to mimic it.
-k <- 50
+k <- 50 
 # the number of factors
+
 constant <- matrix(runif(n*t, -0.5, 0.5), nrow = n, ncol = t)
 #Assume the constant term ( the alpha term from CAPM model) follows uniform distribution
 
@@ -23,47 +24,46 @@ strength <- c( rep(alpha, k),1)
 #the factor strength, with the last term as market factor strength, which equals to 1
 #represents that market factor influence all the strocks, the rest 30 factors have 
 #strength 0.55, which is pretty low.
-critical_value <- qnorm(1-0.025/(2*n^1.96))
 
 Sigma <- diag(1,k+1)
 
 factor <- t(rmvnorm(t,mean = rep(0, k+1), Sigma))
-
+#Generate the factor vector with k factor and one market factor.
+#Each facotr foolows a multinomial distribution with zeor mean and Sigma vairance
 
 
 market_factor <- t(matrix(factor[nrow(factor),]))
 colnames(market_factor) <- paste0("m", 1:t)
 market_factor <- as_tibble(market_factor)%>% 
   pivot_longer(c(paste0("m", 1:t)), names_to = "month", values_to = "market_factor_val")
-#random generate factor list, the last one is the market facotr which by definition
-#equals to the difference between makret return and risk free return
+#Take out the market factor, and pack it into a tibble format.
 
 sig_count <- as.integer((n)^strength)
 #Indicates under the factor strength setting, how many factor laodings are significnatly
 #different from 0. We only take the integer part. From the result we can see that 
-#when the factor strengh is 0.55, 30 amongh 500 stocks will be significant from 0,
-# and when the factor strengh is 1, all 500 loading will be significant
+un
 adj_stat <- pnorm(1-(0.025/(2*n^1.68)))
 
 
 mu_var = 0.71
 
-loading_generate<- function(zero_amount){
+loading_generate<- function(nonzero_amount){
   loading <- matrix(0, n, k+1)
   for(i in 1:(k+1)){
   col <- runif(n,mu_var-0.2, mu_var + 0.2)
-  ind <- which(col %in% sample(col,n-zero_amount[i]))
+  ind <- which(col %in% sample(col,n-nonzero_amount[i]))
   col[ind] <- 0
   loading[,i] <- col
   }
   return(loading)
 }
 loading <- loading_generate(sig_count)
+
 ##Package the generate factor and return into tibble
 return <-  constant + loading%*%factor + error
 colnames(return) <- paste0("m", 1:t)
 return <- as_tibble(return) %>% mutate(stock = 1:n) %>% select(stock, everything())
-return <- return %>%
+ return <- return %>%
   pivot_longer(c(paste0("m", 1:t)), names_to = "month", values_to = "return")
 
 colnames(factor) <- paste0("m", 1:t)
@@ -93,7 +93,9 @@ single_market_result <- single_market_factor %>%
 ##The result is consistent with the inital setting, all the factors loading among 500 assets are significant,
 ##The corresponding strength is 1, indicates the market factor influence across all assets.
 #==============+==============+==============+==============+==============+==============+==============+#
-##The setting of strong factor and one other factor.
+##The setting of market factor and one other factor.
+
+#add market factor as a separate variable of the data frame.
 two_factors <- combine_table %>% 
   filter(factor != k+1) %>% 
   right_join(market_factor, by = 'month') %>% 
@@ -101,15 +103,16 @@ two_factors <- combine_table %>%
 
 two_factor_group<- two_factors %>% 
   group_by(stock,factor) %>% nest()
-
+#Set the regression function
 two_factor_regress <-function(data){
   tidy(lm(return ~ factor_val + market_factor_val, data))
 }
-
+#Run the regression and unnest the results
 two_factor <- two_factor_group %>% 
   mutate(coefficient = map(data, two_factor_regress)) %>% 
   unnest(coefficient)
 
+##Caculate the factor strength
   two_factor_result <- two_factor %>% 
   select(-data) %>% filter(term == "factor_val" ) %>% 
     group_by(factor) %>% filter(statistic > adj_stat) %>% 
@@ -117,10 +120,12 @@ two_factor <- two_factor_group %>%
 
   two_factor_result %>% arrange(alpha_hat)
   summary(two_factor_result)
-  
+  #Calculate the marekt factor's strength under two factor scenario.
   two_factor %>% select(-data) %>% 
     filter(term == "market_factor_val") %>% 
     group_by(factor) %>% 
     filter(statistic > adj_stat) %>% 
     summarize(alpha_hat =(log(n())/log(n))) %>% 
     arrange(alpha_hat)
+  
+  
