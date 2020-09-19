a52cd0de62f51a3e36c3183ec17c3eaab0857f1a
OliverJIang <oliverjiang@outlook.com>
2020-09-19 16:59:42 +1000

    micro improvement

--- Data and Scripts/elastic_net.R
+++ Data and Scripts/elastic_net.R
@@ -83,50 +83,130 @@ select_alpha_table <- select_alpha_table %>%  add_row(combine = "Random", alpha 
 between_05_06 <-   alpha_table %>%  ggplot(aes(x = alpha)) + geom_density()
 between_07_08 <-   alpha_table %>%  ggplot(aes(x = alpha)) + geom_density()
 above_09 <- alpha_table %>%  ggplot(aes(x = alpha)) + geom_density()
-above_09
+random_plot <-alpha_table %>%  ggplot(aes(x = alpha)) + geom_density()
 #=============#=============#=============#=============#=============#=============#
+learning_data <- semi_join(thirty_factor_return, thirty_strength %>% dplyr::filter(strength >=0.9& strength <1), by = "Factor") %>% 
+  #dplyr::filter(Factor %in% sample(unique(Factor),10)) %>% 
+  #dplyr::filter(Ticker %in% sample(unique(Ticker),30)) %>%   
+  tidyr::pivot_wider(names_from = Factor, values_from = Value)%>% 
+  dplyr::select(-c(Date, Return))
 
-learning_data <- semi_join(thirty_factor_return, thirty_strength %>% filter(strength >=0.9), by = "Factor") %>% 
-  #filter(Factor %in% sample(unique(Factor),10)) %>% 
-  filter(Ticker %in% sample(unique(Ticker), 50)) %>%   
-  pivot_wider(names_from = Factor, values_from = Value)%>% 
-  select(-c(Date, Return))
+learning_data <-semi_join(thirty_factor_return, thirty_strength %>% dplyr::filter(strength >=0.9& strength <1), by = "Factor")  %>% 
+  dplyr::filter(Factor %in% sample(unique(Factor),10)) %>% 
+  bind_rows(semi_join(thirty_factor_return, thirty_strength %>% dplyr::filter(strength >=0& strength <0.5), by = "Factor") %>% 
+               dplyr::filter(Factor %in% sample(unique(Factor),10))) %>% 
+  tidyr::pivot_wider(names_from = Factor, values_from = Value)%>% 
+  dplyr::select(-c(Date, Return))
 
-learning_feature_matrix<- learning_data %>% 
+learning_feature_matrix <- learning_data %>% 
   dplyr::select(-c(Ticker,Excess,Market)) %>% 
   as.matrix() 
-learning_resid_matrix<- (lm(Excess ~ Market, learning_data) %>% 
+learning_resid_matrix <- (lm(Excess ~ Market, learning_data) %>% 
                            augment())$.resid %>% as.matrix()
 
+# Apply the elastic net model
+en_alpha <- 0.448
+mix_en_result <-  auto_count_select(learning_data, alpha = en_alpha) 
+mix_en_model <- cv.glmnet(learning_feature_matrix, learning_resid_matrix, alpha = en_alpha)
+mix_en_coef <- coef(mix_en_model, s = "lambda.min") 
+mix_en_matrix <-  coef_determine(learning_data, alpha = en_alpha) 
 
-mod_result <-  auto_count_select(learning_data, alpha = 1) 
-learning_result <- cv.glmnet(learning_feature_matrix, learning_resid_matrix, alpha = 1)
-mod_result %>% arrange(desc(count)) 
-mod_result %>% arrange(count) 
+mix_en_company_count <-mix_en_matrix %>% dplyr::filter(Origin != "(Intercept)") %>% 
+  pivot_longer(!Origin, names_to = "Ticker", values_to = "Coeff") %>% 
+  mutate(none_zero = ifelse(Coeff == 0, 0,1), deno = length(unique(Origin))) %>% 
+  group_by(Ticker) %>% 
+  summarise(prop = (sum(none_zero)/mean(deno))) %>% 
+  arrange(desc(prop))
 
-learning_coef <- coef(learning_result, s = "lambda.min") 
-test_matrix   <-  coef_determine(learning_data, alpha = 1) 
-
-
-
-xtable(thirty_strength %>% filter(Factor %in% mod_result$Factor) %>% right_join(mod_result, by= "Factor") %>% arrange(desc(count)) %>% 
-         select(Factor, prop, strength, pi),
-       digits = c(0,0,3,3,3))
-
-two_thirty_count <- given_count_select(two_thirty_factor_return,alpha = 0.54, lambda = learning_lambda, strength_threshold = 0.7 )
-auto_two_thirty_count <- auto_count_select(two_thirty_factor_return,alpha = 0.54,  strength_threshold = 0.7 )
-two_thirty_count %>% arrange(desc(count))
-auto_two_thirty_count %>% arrange(desc(count))
-
-three_thirty_count <- given_count_select(three_thirty_factor_return,alpha = 0.54, lambda = learning_lambda, strength_threshold = 0 )
-auto_three_thirty_count <- auto_count_select(three_thirty_factor_return,alpha = 0.54,  strength_threshold = 0 )
-three_thirty_count %>% arrange(desc(count))
-auto_three_thirty_count %>% arrange(desc(count))
+mix_en_company_count %>% 
+  ggplot(aes(x = prop)) +
+  geom_bar()+
+  ggtitle("Elastic Net Result")
 
 
-ten_count <- given_count_select(ten_factor_return,alpha = 0.54, lambda = learning_lambda, strength_threshold = 0 )
-auto_ten_count <- auto_count_select(ten_factor_return,alpha = 0.54,  strength_threshold = 0 )
+# Apply the lasso model
+mix_lasso_result <-  auto_count_select(learning_data, alpha = 1) 
+mix_lasso_model <- cv.glmnet(learning_feature_matrix, learning_resid_matrix, alpha = 1)
+mix_lasso_coef <- coef(mix_lasso_model, s = "lambda.min") 
+mix_lasso_matrix   <- coef_determine(learning_data, alpha = 1) 
 
-two_thirty_count %>% view()
+mix_lasso_company_count <-mix_lasso_matrix %>% dplyr::filter(Origin != "(Intercept)") %>% 
+  pivot_longer(!Origin, names_to = "Ticker", values_to = "Coeff") %>% 
+  mutate(none_zero = ifelse(Coeff == 0, 0,1), deno = length(unique(Origin))) %>% 
+  group_by(Ticker) %>% 
+  summarise(prop = (sum(none_zero)/mean(deno))) %>% 
+  arrange(desc(prop))
+
+mix_lasso_company_count %>% 
+  ggplot(aes(x = prop)) +
+  geom_bar()+
+  ggtitle("Lasso Result")
 
 
+mix_quasi_ridge_result  <-  auto_count_select(learning_data, alpha = 0.1) 
+mix_quasi_ridge_model <- cv.glmnet(learning_feature_matrix, learning_resid_matrix, alpha = 0.1)
+mix_quasi_ridge_coef <- coef(mix_quasi_ridge_model, s = "lambda.min") 
+mix_quasi_ridge_matrix   <- coef_determine(learning_data, alpha = 0.1) 
+
+mix_quasi_ridge_company_count <-mix_quasi_ridge_matrix %>% dplyr::filter(Origin != "(Intercept)") %>% 
+  pivot_longer(!Origin, names_to = "Ticker", values_to = "Coeff") %>% 
+  mutate(none_zero = ifelse(Coeff == 0, 0,1), deno = length(unique(Origin))) %>% 
+  group_by(Ticker) %>% 
+  summarise(prop = (sum(none_zero)/mean(deno))) %>% 
+  arrange(desc(prop))
+
+mix_quasi_ridge_company_count %>% 
+  ggplot(aes(x = prop)) +
+  geom_bar()+
+  ggtitle("Small alpha Elastic Net")
+
+
+
+
+# Apply the ridge model
+ridge_result <-  auto_count_select(learning_data, alpha = 0) 
+ridge_model <- cv.glmnet(learning_feature_matrix, learning_resid_matrix, alpha = 0)
+ridge_coef <- coef(ridge_model, s = "lambda.min") 
+ridge_matrix   <-  coef_determine(learning_data, alpha = 0) 
+
+ridge_company_count <-ridge_matrix %>% filter(Origin != "(Intercept)") %>% 
+  pivot_longer(!Origin, names_to = "Ticker", values_to = "Coeff") %>% 
+  mutate(none_zero = ifelse(Coeff == 0, 0,1), deno = length(unique(Origin))) %>% 
+  group_by(Ticker) %>% 
+  summarise(prop = (sum(none_zero)/mean(deno))) %>% 
+  arrange(desc(prop))
+
+ridge_company_count %>% 
+  ggplot(aes(x = prop)) +
+  geom_histogram()+
+  ggtitle("Ridge Regression")
+
+
+
+en_company_count %>% 
+  left_join(lasso_company_count, by = "Ticker") %>%
+  left_join(asy_ridge_company_count, by = "Ticker") %>% 
+  select(everything(),en = prop.x, lasso = prop.y, asy_ridge = prop) %>% 
+  pivot_longer(c("en", "lasso", "asy_ridge")) %>% 
+  ggplot(aes(x = value))+
+  geom_bar()+
+  facet_wrap(.~name)
+  geom_density(aes(color = name))
+
+  
+  zero_counter(en_matrix)
+  zero_counter(lasso_matrix)
+  
+  
+  #comp_0005<- matrix_compare(en_matrix, lasso_matrix) %>% mutate(toler = ifelse(same_call>= floor((ncol(learning_data) - 3) *0.9), 1,0),
+   #                                                             exact = ifelse(same_call == (ncol(learning_data)-3), 1, 0))
+
+  
+  comp_mix  
+  comp_0910
+  comp_0809
+  comp_0708
+  comp_0607
+  comp_0506
+  sum(comp_0005$toler)/243
+  sum(comp_0005$exact)/243
